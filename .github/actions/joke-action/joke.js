54503a2d74d23875e32b68a58692e329fbe2ff65
zaknafien99@hotmail.com <zaknafien99@hotmail.com>
2026-04-04 12:08:55 +1000

    Fix issue #7: Update to Node.js 20 and modernize HTTP requests to resolve Undici vulnerabilities

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,18 +1,12 @@
-const request = require("request-promise");
-
-const options = {
-  method: "GET",
-  uri: "https://icanhazdadjoke.com/",
-  headers: {
-    Accept: "application/json",
-    "User-Agent": "Writing JavaScript action GitHub Skills course.",
-  },
-  json: true,
-};
-
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "Writing JavaScript action GitHub Skills course.",
+    },
+  });
+  const data = await response.json();
+  return data.joke;
 }
 
 module.exports = getJoke;
